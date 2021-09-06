0df5190db99fd92724f527123da6033b44feeede
Aman Dixit <amandikshit10@gmail.com>
2021-09-06 23:12:03 +0530

    add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
 import HeroSection from '../components/HeroSection';
-import PText from '../components/PText';
 import ServiceSection from '../components/ServiceSection';
 import ProjectSection from '../components/ProjectSection';
 
