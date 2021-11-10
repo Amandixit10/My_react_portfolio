88928d12d0d4938bbe0d3bc6a93e88d935381c15
Aman Dixit <amandikshit10@gmail.com>
2021-11-10 19:34:27 +0530

    add

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,6 +11,7 @@ import Buttons from '../components/Buttons';
 import AboutImg from '../assets/images/about-page-img.png'
 import AboutinfoItem from '../components/AboutinfoItem';
 import ContactBanner from '../components/ContactBanner';
+import Footer from '../components/Footer';
 
 const AboutPageStyles=styled.div`
 padding: 20rem 10rem 0;
@@ -81,70 +82,64 @@ padding: 20rem 10rem 0;
 
 export default function About() {
     return (
-        <AboutPageStyles>
+        <><AboutPageStyles>
             <div className="container">
                 <div className="top-section">
                     <div className="left">
                         <p className="about__subheading">
                             Hi, I am <span>Aman Dixit</span>
-                            </p>
-                            <h2 className="about__heading">A freelance Web developer</h2>
-                            <div className="about__info">
-                                <PText>
-I am a pre-final year student at GGSIPU, Delhi. Since my 
-childhood i love innovation and design. I always try to 
-think the science behind the working of every perticular
- thing that the human race have deveoleped .
-<br/> <br/>
- I started coding a bit late, in my sophomore year. I started 
- with basic DSA and then moved on to CP. I enjoyed problem 
- solving and in CP, there is time constraint so, thinking about
- a solution in a limited period of time is even more exciting.
-            </PText>
+                        </p>
+                        <h2 className="about__heading">A freelance Web developer</h2>
+                        <div className="about__info">
+                            <PText>
+                                I am a pre-final year student at GGSIPU, Delhi. Since my
+                                childhood i love innovation and design. I always try to
+                                think the science behind the working of every perticular
+                                thing that the human race have deveoleped .
+                                <br /> <br />
+                                I started coding a bit late, in my sophomore year. I started
+                                with basic DSA and then moved on to CP. I enjoyed problem
+                                solving and in CP, there is time constraint so, thinking about
+                                a solution in a limited period of time is even more exciting.
+                            </PText>
+                        </div>
+                        <Buttons btnText="Download CV" btnLink="#" />
+                    </div>
+                    <div className="right">
+                        <img src={AboutImg} alt="This is my img" />
+                    </div>
+                </div>
+                <div className="about__info__items">
+                    <div className="about__info__item">
+                        <h1 className="about__info__heading">Education</h1>
+                        <AboutinfoItem
+                            title="School"
+                            items={['Vivekanand School, Anand Vihar, Delhi']} />
+                        <AboutinfoItem
+                            title="College"
+                            items={['Maharaja Agrasen Institute Of Technology']} />
+                        <AboutinfoItem
+                            title="Varsity"
+                            items={['GGSIPU, Dwarka, Delhi']} />
+                    </div>
+                </div>
+                <div className="about__info__items">
+                    <div className="about__info__item">
+                        <h1 className="about__info__heading">My Skill</h1>
+                        <AboutinfoItem
+                            title="FrontEnd"
+                            items={['HTML', 'JS', 'CSS', 'REACT']} />
+                        <AboutinfoItem
+                            title="BackEnd"
+                            items={['NODE', 'EXPRESS', 'MONGODB']} />
+                        <AboutinfoItem
+                            title="Design"
+                            items={['Photoshop', 'Figma', 'After Effects']} />
+                    </div>
+                </div>
             </div>
-            <Buttons btnText="Download CV" btnLink="#"/>
-        </div>
-        <div className="right">
-            <img src={AboutImg} alt="This is my img"/>
-        </div>
-        </div>
-        <div className="about__info__items">
-            <div className="about__info__item">
-             <h1 className="about__info__heading">Education</h1>  
-             <AboutinfoItem
-             title="School" 
-             items={['Vivekanand School, Anand Vihar, Delhi']}
-             />
-              <AboutinfoItem
-             title="College" 
-             items={['Maharaja Agrasen Institute Of Technology']}
-             />
-             <AboutinfoItem
-             title="Varsity" 
-             items={['GGSIPU, Dwarka, Delhi']}
-             />
-            </div>
-        </div>
-        <div className="about__info__items">
-            <div className="about__info__item">
-             <h1 className="about__info__heading">My Skill</h1>  
-             <AboutinfoItem
-             title="FrontEnd" 
-             items={['HTML','JS','CSS','REACT']}
-             />
-              <AboutinfoItem
-             title="BackEnd" 
-             items={['NODE','EXPRESS','MONGODB']}
-             />
-             <AboutinfoItem
-             title="Design" 
-             items={['Photoshop','Figma','After Effects']}
-             />
-            </div>
-        </div>
-        </div>
-        <ContactBanner/>
-        </AboutPageStyles>
+            <ContactBanner />
+        </AboutPageStyles><Footer /></>
     )
 }
 
