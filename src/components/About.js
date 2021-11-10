d4d2bf38777d7a7c07c52c8becfba2abb07a0338
Aman Dixit <amandikshit10@gmail.com>
2021-11-10 12:53:05 +0530

    add

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable prettier/prettier */
+import React from 'react'
+
+export default function About() {
+    return (
+        // eslint-disable-next-line react/self-closing-comp
+        <div>
+           <div className="container">
+               <div className="top-section">
+                   <div className="left">
+                       <p className="about__subheading">Hi, I am<span>Aman Dixit</span></p>
+                       <h2 className="about__info"></h2>
+                   </div>
+               </div>
+               </div> 
+        </div>
+    )
+}
